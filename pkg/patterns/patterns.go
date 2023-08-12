2cbb9a896d99bb8311449d1f0770f6cdf4203b85
Дмитрий Титов <dtsp@yandex.ru>
2023-08-12 15:43:23 +0300

    Initial Project Structure

--- pkg/patterns/patterns.go
+++ pkg/patterns/patterns.go
@@ -1,5 +1,27 @@
 package patterns
 
+import "sync"
+
+// FanIn pattern generic implementation.
 func FanIn[T any](chans ...chan T) chan T {
-	return nil
+	out := make(chan T)
+
+	var wg sync.WaitGroup
+
+	for _, ch := range chans {
+		go func(ch chan T) {
+			for val := range ch {
+				out <- val
+			}
+
+			wg.Done()
+		}(ch)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
 }
